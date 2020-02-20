d3e4bef8ea2228c57ff1858156a709f90fa67186
j <lorzechowski@protonmail.com>
2020-02-20 18:55:25 +0100

    build

---
+++ docs/precache-manifest.1ed5c6fb02675166ef2ed6b91383a3cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026ad58c870ae53f63d47e678996e547",
+    "url": "/songsterr-search-app/index.html"
+  },
+  {
+    "revision": "78221ecf8878d532581b",
+    "url": "/songsterr-search-app/static/css/main.ab09aa44.chunk.css"
+  },
+  {
+    "revision": "84f9120bc8b945d8670f",
+    "url": "/songsterr-search-app/static/js/2.9abd5f52.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/songsterr-search-app/static/js/2.9abd5f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78221ecf8878d532581b",
+    "url": "/songsterr-search-app/static/js/main.a8fea3b0.chunk.js"
+  },
+  {
+    "revision": "62ec18c361af3cd194d3",
+    "url": "/songsterr-search-app/static/js/runtime-main.dd3a62dc.js"
+  },
+  {
+    "revision": "16aeeab7c1fcdf3b5a1db47d7cf5b534",
+    "url": "/songsterr-search-app/static/media/search.16aeeab7.svg"
+  }
+]);
